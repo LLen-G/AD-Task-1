90da29ac2744085ae6ee5769f0187b1c3a7b8ed0
MementoLLenA <167205270+LLen-G@users.noreply.github.com>
2025-05-14 00:01:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Honk&family=Share+Tech&display=swap" rel="stylesheet">
+    <title>AD-Task-1</title>
+</head>
+
+<body>
+    <div class="main">
+        <h1>Welcome to AD-Task-1!</h1>
+        <p>Create a sample project that displays the basics of programming. designed and themed based on your own min of 2 pages and applying the rules</p>
+        Basics:
+        <ul>
+            <li>Declarations</li>
+            <li>Conditionals</li>
+            <li>Looping</li>
+        </ul>
+        <a href="Pages/Page1.php" class="btn-link">Let's Get Started</a>
+    </div>
+
+</body>
+
+</html>
